a3c1d7093a1385e91c4dee9da63bcaa1b2d5a0c3
MD <cossoalkahn@yahoo.com>
2019-05-18 02:31:06 -0400

    Lab 3
    Particle emiiter where  pressing W or S will result in changes to gravity effect on the particles. Particles change colors when they collide and change over time

---
+++ cse3541lab3-master/Assets/Resources/Scripts/Emitter.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Emitter : MonoBehaviour {
+
+    public static float FORCE_MAGNITUDE = 15.0f;
+    public static float MAX_SPEED = 5f;
+    public static float MIN_SPEED = 2.5f;
+
+    public Particle Particle;
+    public int MaxNumOfParticles = 20;
+    public float DeltaTime = 8.0f;
+    public float Threshold = 10.0f;
+
+    private List<Particle> particles;
+
+	// Use this for initialization
+	void Start () {
+        particles = new List<Particle>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        DeltaTime += Time.deltaTime;
+        if (DeltaTime > Threshold) {
+            DeltaTime = 0.0f;
+            GenerateParticles();
+        }
+        foreach (Particle p in particles)
+        {
+            UpdateParticle(p, Time.deltaTime);
+        }
+        particles.RemoveAll(IsDisposable);
+    }
+
+    private bool IsDisposable(Particle p) {
+        return p.Disposable;
+    }
+
+    void GenerateNewParticle() {
+        Particle p = Particle.Create(new Vector3(0, 3, 0), new Vector3(1, 1, 1), 10.0f);
+        particles.Add(p);
+    }
+
+    void UpdateParticle(Particle p, float deltaTime) {
+        p.Age += deltaTime;
+        if (p.Age > p.MaxAge)
+        {
+            p.Disposable = true;
+        }
+        p.ResetForce();
+		HandleParticleParticleCollision ();
+        p.UpdatePhysics(deltaTime);
+    }
+
+    void HandleParticleParticleCollision(){
+		foreach (Particle p in particles) {
+			foreach (Particle q in particles) {
+				if (!p.Equals (q)) {
+					float distance = Mathf.Pow (p.center.x - q.center.x, 2) + Mathf.Pow (p.center.y - q.center.y, 2) + Mathf.Pow(p.center.z - q.center.z, 2);
+					if (Mathf.Sqrt (distance) <= (p.radius + q.radius)) {
+                        Vector3 pForceDirection = p.center - q.center;
+                        Vector3 qForceDirection = -pForceDirection;
+
+                        p.ApplyForce(pForceDirection * FORCE_MAGNITUDE);
+                        q.ApplyForce(qForceDirection * FORCE_MAGNITUDE);
+
+					}
+				}
+			}
+		}
+	}
+
+    void GenerateParticles() {
+        for (int i = 0; i < this.MaxNumOfParticles; i++) {
+            Vector3 origin = new Vector3(0, 0.75f, 0);
+            Vector3 pos = GetRandomPosition(origin);
+			Particle p = Particle.Create(pos, GetRandomVelocity(origin, pos), Random.Range(5.0f, 15.0f));
+            particles.Add(p);
+        }
+    }
+
+    Vector3 GetRandomPosition(Vector3 origin) {
+        float posX = origin.x + Random.Range(-0.5f, 0.5f);
+        float posY = origin.y + Random.Range(0.0f, 0.5f);
+        float posZ = origin.z + Random.Range(-0.5f, 0.5f);
+
+        while (Mathf.Sqrt(Mathf.Pow(posX - origin.x, 2) + Mathf.Pow(posY - origin.y, 2) + Mathf.Pow(posZ - origin.z, 2)) > 0.5f) {
+			posX = origin.x + Random.Range(-0.5f, 0.5f);
+			posY = origin.y + Random.Range(0.0f, 0.5f);
+			posZ = origin.z + Random.Range(-0.5f, 0.5f);
+        }
+
+        return new Vector3(posX, posY, posZ);
+    }
+
+    Vector3 GetRandomVelocity(Vector3 origin, Vector3 pos) {
+        Vector3 direction = (pos - origin).normalized;
+        return direction * Random.Range(MIN_SPEED, MAX_SPEED);
+    }
+}
---
+++ cse3541lab3-master/Assets/Resources/Scripts/Particle.cs
@@ -0,0 +1,184 @@
+using UnityEngine;
+
+public class Particle : MonoBehaviour {
+    public Vector3 Position;
+    public Vector3 Velocity;
+    public Vector3 Gravity;
+    public float Age;
+    public float MaxAge = 5.0f;
+    public float Mass = 1.0f;
+	public float VelocityThreshold = 0.01f;
+
+	public Vector3 center;
+	public float radius;
+    public GameObject Apperance;
+    public GameObject BackWall;
+    public GameObject FrontWall;
+    public GameObject LeftWall;
+    public GameObject RightWall;
+    public GameObject Ceiling;
+    public GameObject Floor;
+
+    public bool Disposable;
+
+    private Vector3 Force;
+    private Renderer rend;
+    private Light light;
+
+    public static Particle Create(Vector3 position, Vector3 velocity, float maxAge) {
+        GameObject obj = Instantiate(Resources.Load("Prefabs/Particle")) as GameObject;
+        Particle particle = obj.GetComponent<Particle>();
+        particle.Init(position, velocity, maxAge);
+        return particle;
+    }
+
+    public void Init(Vector3 position, Vector3 velocity, float maxAge) {
+        this.Position = position;
+        this.Velocity = velocity;
+        this.Age = 0.0f;
+        this.Force = this.Gravity;
+        this.MaxAge = maxAge;
+    }
+
+    // Use this for initialization
+    void Start () {
+        Disposable = false;
+        this.Force = this.Gravity;
+        BackWall = GameObject.Find("BackWall");
+        FrontWall = GameObject.Find ("FrontWall");
+        LeftWall = GameObject.Find ("LeftWall");
+        RightWall = GameObject.Find ("RightWall");
+        Ceiling = GameObject.Find ("Ceiling");
+        Floor = GameObject.Find ("Floor");
+        this.rend = GetComponent<Renderer>();
+        this.light = GetComponent<Light>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        CollisionDetection();
+        UpdateColor();
+        if (Disposable) {
+            Destroy(gameObject);
+        }
+    }
+
+    void UpdateColor() {
+        Color newColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        rend.material.color = newColor;
+        light.color = newColor;
+    }
+
+    public void UpdatePhysics(float deltaTime) {
+        this.Position = CalculateNewPosition(deltaTime);
+		this.Velocity = (this.Velocity.magnitude <= this.VelocityThreshold) ? Vector3.zero : CalculateNewVelocity(deltaTime);
+        this.transform.position = this.Position;
+    }
+
+    public void ApplyForce(Vector3 force) {
+        this.Force += force;
+    }
+
+    public void ResetForce() {
+        this.Force = this.Gravity;
+    }
+
+    private Vector3 CalculateNewPosition(float deltaTime)
+    {
+        return this.Position + CalculateNewVelocity(deltaTime / 2) * deltaTime;
+    }
+    
+    private Vector3 CalculateNewVelocity(float deltaTime)
+    {
+        return this.Velocity + CalculateAcceleration() * deltaTime;
+    }
+
+    private Vector3 CalculateAcceleration() {
+        return this.Force / this.Mass;
+    }
+
+    private void CollisionDetection(){
+        
+        float floor_bound_y = Floor.transform.position.y + Floor.transform.localScale.y / 2;
+        float ceiling_bound_y = Ceiling.transform.position.y - Ceiling.transform.localScale.y / 2;
+        float rightWall_bound_x = RightWall.transform.position.x - RightWall.transform.localScale.x / 2;
+        float leftWall_bound_x = LeftWall.transform.position.x + LeftWall.transform.localScale.x / 2;
+        float frontWall_bound_z = FrontWall.transform.position.z - FrontWall.transform.localScale.z / 2;
+        float backWall_bound_z = BackWall.transform.position.z + BackWall.transform.localScale.z / 2;
+        
+        Matrix4x4 localToWorld = transform.localToWorldMatrix;
+        Vector3[] local_vertices = this.GetComponent<MeshFilter>().mesh.vertices;
+        Vector3[] world_vertices = new Vector3[local_vertices.Length];
+        for (int i = 0; i < local_vertices.Length; i++) {
+            world_vertices [i] = localToWorld.MultiplyPoint3x4(local_vertices[i]);
+        }
+        
+        float sum_x = 0f, sum_y = 0f, sum_z = 0f;
+        for (int i = 0; i < local_vertices.Length; i++) {
+            sum_x += world_vertices [i].x;
+            sum_y += world_vertices [i].y;
+            sum_z += world_vertices [i].z;
+        }
+        
+        center = new Vector3 (sum_x / world_vertices.Length, sum_y / world_vertices.Length, sum_z / world_vertices.Length);
+        radius = 0f;
+        for (int i = 0; i < world_vertices.Length; i++) {
+            float distance = Mathf.Pow ((world_vertices [i].x - center.x), 2) + Mathf.Pow ((world_vertices [i].y - center.y), 2) + Mathf.Pow ((world_vertices [i].z - center.z), 2);
+            if (distance > Mathf.Pow(radius, 2))
+            radius = Mathf.Sqrt(distance);
+        }
+        
+        if (Mathf.Abs ((float)(center.y - floor_bound_y)) <= radius || center.y < floor_bound_y) {
+            Vector3 newVelocity = this.Velocity;
+            Vector3 newPosition = this.Position;
+            newVelocity.y = -(newVelocity.y * 0.8f);
+            newPosition.y = floor_bound_y + radius;
+            this.Position = newPosition;
+            this.Velocity = newVelocity;
+        }
+		if (Mathf.Abs ((float)(center.y - ceiling_bound_y)) <= radius || center.y > ceiling_bound_y) {
+            Vector3 newVelocity = this.Velocity;
+			Vector3 newPosition = this.Position;
+			newVelocity.y = -(newVelocity.y * 0.8f);
+			newPosition.y = ceiling_bound_y - radius;
+			this.Position = newPosition;
+			this.Velocity = newVelocity;
+        }
+		if (Mathf.Abs ((float)(center.x - rightWall_bound_x)) <= radius || center.x > rightWall_bound_x) {
+            Vector3 newVelocity = this.Velocity;
+			Vector3 newPosition = this.Position;
+            newVelocity.x = -(newVelocity.x * 0.8f);
+			newPosition.x = rightWall_bound_x - radius;
+			this.Position = newPosition;
+            this.Velocity = newVelocity;
+        }
+		if (Mathf.Abs ((float)(center.x - leftWall_bound_x)) <= radius || center.x < leftWall_bound_x) {
+			Vector3 newVelocity = this.Velocity;
+			Vector3 newPosition = this.Position;
+			newVelocity.x = -(newVelocity.x * 0.8f);
+			newPosition.x = leftWall_bound_x + radius;
+			this.Position = newPosition;
+			this.Velocity = newVelocity;
+        }
+		if (Mathf.Abs ((float)(center.z - frontWall_bound_z)) <= radius || center.z > frontWall_bound_z) {
+            Vector3 newVelocity = this.Velocity;
+			Vector3 newPosition = this.Position;
+            newVelocity.z = -(newVelocity.z*0.8f);
+			newPosition.z = frontWall_bound_z - radius;
+			this.Position = newPosition;
+            this.Velocity = newVelocity;
+        }
+		if (Mathf.Abs ((float)(center.z - backWall_bound_z)) <= radius || center.z < backWall_bound_z) {
+            Vector3 newVelocity = this.Velocity;
+			Vector3 newPosition = this.Position;
+            newVelocity.z = -(newVelocity.z*0.8f);
+			newPosition.z = backWall_bound_z + radius;
+			this.Position = newPosition;
+            this.Velocity = newVelocity;
+        }
+        
+        
+        
+    }
+    
+}
